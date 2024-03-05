6c4cf6b88c1436a7783f508a832462daa6a52a89
phetlada  thongkarn <phetlada@phetladas-MacBook-Pro.local>
2024-03-05 23:08:09 +0700

    feat: style design card work exp and sentence

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,65 @@ export default {
   data() {
     return {
       toggleA: true,
-      toggleB: true
+      toggleB: true,
+      data_project_intren : {
+        company:"Creden Asia Co., Lid.",
+        career:"Software Deverloper Internship",
+        time:"May. 2021 - Oct. 2021",
+        content_card : [
+          {
+            topic:"Landing Page Development",
+            detail_topic : [
+              "Write clean and efficient HTML, CSS, and JavaScript code to create beautiful and user-friendly landing pages.",
+              "Collaborate with UI/UX designers to deliver landing pages that meet user needs."
+            ]
+          },
+          {
+            topic:"API Development",
+            detail_topic : [
+              "Design and build new APIs based on business requirements.",
+              "Write API code in a suitable language (e.g., Lua,Python,Node.js).",
+              "Create API documentation for developers."
+            ]
+          },
+          {
+            topic:"Developed a Back-Office Project",
+            detail_topic : [
+              "Developed a back-office system to manage and organize data for a company.",
+              "The system was designed to be user-friendly and efficient, allowing users to easily manage their data.",
+            ]
+          }
+        ]
+      },
+      data_project_software_dev : {
+        company:"Creden Asia Co., Lid.",
+        career:"Software Deverloper",
+        time:"Jun. 2022 - Present.",
+        content_card : [
+          {
+            topic:"Website Developer",
+            detail_topic : [
+              "Write clean and efficient HTML, CSS, and JavaScript code to create beautiful and user-friendly landing pages.",
+              "Collaborate with UI/UX designers to deliver landing pages that meet user needs."
+            ]
+          },
+          {
+            topic:"Web scraping",
+            detail_topic : [
+              "Design and build new APIs based on business requirements.",
+              "Write API code in a suitable language (e.g., Lua,Python,Node.js).",
+              "Create API documentation for developers."
+            ]
+          },
+          {
+            topic:"Data Cleaning",
+            detail_topic : [
+              "Developed a back-office system to manage and organize data for a company.",
+              "The system was designed to be user-friendly and efficient, allowing users to easily manage their data.",
+            ]
+          }
+        ]
+      }
     };
   }
 }
@@ -69,34 +127,35 @@ export default {
       </div>
       <div class="bg-content-work-experience">
         <div class="box-content">
-          <h1 class="topic-content">Work Experience</h1>
-          <div class="decs-topic text-xl">
-            <span class="flex justify-between">
-              <svg class="h-8 w-8 text-yellow-500" width="24" height="24" viewBox="0 0 24 24"
-                stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" />
-                <path d="M8 9l5 5v7h-5v-4m0 4h-5v-7l5 -5m1 1v-6a1 1 0 0 1 1 -1h10a1 1 0 0 1 1 1v17h-8" />
-                <line x1="13" y1="7" x2="13" y2="7.01" />
-                <line x1="17" y1="7" x2="17" y2="7.01" />
-                <line x1="17" y1="11" x2="17" y2="11.01" />
-                <line x1="17" y1="15" x2="17" y2="15.01" />
-              </svg>
-              | May. 2021 - Oct. 2021
-            </span>
-            Software Deverloper Internship
-            <div class="text-lg">Creden Asia Co., Lid.</div>
-          </div>
           <transition name="slide" appear>
             <div class="side-content" v-if="toggleB">
-              <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-                <card>
-                  <img src="./assets/landing-page.webp" />
+              <div class="topic-content text-white">Work Experience</div>
+              <div class="decs-topic">
+                <span class="flex justify-between text-lg md:text-xl">
+                  <svg class="h-8 w-8 text-yellow-500" width="24" height="24" viewBox="0 0 24 24"
+                    stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" />
+                    <path d="M8 9l5 5v7h-5v-4m0 4h-5v-7l5 -5m1 1v-6a1 1 0 0 1 1 -1h10a1 1 0 0 1 1 1v17h-8" />
+                    <line x1="13" y1="7" x2="13" y2="7.01" />
+                    <line x1="17" y1="7" x2="17" y2="7.01" />
+                    <line x1="17" y1="11" x2="17" y2="11.01" />
+                    <line x1="17" y1="15" x2="17" y2="15.01" />
+                  </svg>
+                  | {{ data_project_intren.time }}
+                </span>
+                {{ data_project_intren.career }}
+                <div class=" md:text-lg text-base">{{ data_project_intren.company }}</div>
+              </div>
+              <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10">
+
+                <card v-for="(value,index) in data_project_intren.content_card" :key="value.topic">
+                  <img v-if="index == 0" class="rounded" src="./assets/landing-page.webp" />
+                  <img v-if="index == 1" class="rounded" src="./assets/task-api.webp" />
+                  <img v-if="index == 2" class="rounded" src="./assets/project-backoffice.webp" />
                   <div class="content-card">
-                    <span class="font-bold font-xl">Landing Page Development</span>
-                    <ul class="list-disc ml-8 text-base">
-                      <!-- <li>Build new landing pages based on business requirements.</li> -->
-                      <li>Write clean and efficient HTML, CSS, and JavaScript code to create beautiful and user-friendly landing pages.</li>
-                      <li>Collaborate with UI/UX designers to deliver landing pages that meet user needs.</li>
+                    <span class="font-bold font-xl">{{ value.topic }}</span>
+                    <ul class="list-disc text-ellipsis ml-8 text-base">
+                      <li v-for="sentence in value.detail_topic">{{ sentence  }}</li>
                     </ul>
                   </div>
                   <div class="bottom-card flex justify-end">
@@ -107,14 +166,35 @@ export default {
                     </button>
                   </div>
                 </card>
-                <card>
-                  <img src="./assets/landing-page.webp" />
+
+              </div>
+
+              <div class="decs-topic mt-10">
+                <span class="flex justify-between text-lg md:text-xl">
+                  <svg class="h-8 w-8 text-yellow-500" width="24" height="24" viewBox="0 0 24 24"
+                    stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" />
+                    <path d="M8 9l5 5v7h-5v-4m0 4h-5v-7l5 -5m1 1v-6a1 1 0 0 1 1 -1h10a1 1 0 0 1 1 1v17h-8" />
+                    <line x1="13" y1="7" x2="13" y2="7.01" />
+                    <line x1="17" y1="7" x2="17" y2="7.01" />
+                    <line x1="17" y1="11" x2="17" y2="11.01" />
+                    <line x1="17" y1="15" x2="17" y2="15.01" />
+                  </svg>
+                  | {{ data_project_software_dev.time }}
+                </span>
+                {{ data_project_software_dev.career }}
+                <div class=" md:text-lg text-base">{{ data_project_software_dev.company }}</div>
+              </div>
+              <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10">
+
+                <card v-for="(value,index) in data_project_software_dev.content_card" :key="value.topic">
+                  <img v-if="index == 0" class="rounded" src="./assets/full-stack.webp" />
+                  <img v-if="index == 1" class="rounded" src="./assets/wep-scraping.webp" />
+                  <img v-if="index == 2" class="rounded" src="./assets/data-cleansing.webp" />
                   <div class="content-card">
-                    <span class="font-bold font-xl">API Development</span>
-                    <ul class="list-disc text-base ml-8">
-                      <li>Design and build new APIs based on business requirements.</li>
-                      <li>Write API code in a suitable language (e.g., Lua,Python,Node.js).</li>
-                      <li>Create API documentation for developers.</li>
+                    <span class="font-bold font-xl">{{ value.topic }}</span>
+                    <ul class="list-disc text-ellipsis ml-8 text-base">
+                      <li v-for="sentence in value.detail_topic">{{ sentence  }}</li>
                     </ul>
                   </div>
                   <div class="bottom-card flex justify-end">
@@ -125,23 +205,7 @@ export default {
                     </button>
                   </div>
                 </card>
-                <card>
-                  <img src="./assets/landing-page.webp" />
-                  <div class="content-card">
-                    <span class="font-bold font-xl">Developed a Back-Office Project</span>
-                    <ul class="list-disc ml-8 text-base">
-                      <li>Developed a back-office system to manage and organize data for a company.</li>
-                      <li>The system was designed to be user-friendly and efficient, allowing users to easily manage their data.</li>
-                    </ul>
-                  </div>
-                  <div class="bottom-card flex justify-end">
-                    <button
-                      class="align-middle select-none font-sans font-bold text-center uppercase"
-                      type="button">
-                      Read More
-                    </button>
-                  </div>
-                </card>
+
               </div>
             </div>
           </transition>
@@ -163,6 +227,7 @@ button:hover {
   background-color: #608fbd;
   padding: 2%;
   border-radius: 10px;
+  transform: scale(1.2) perspective(2px)
 }
 .slide-enter {
   opacity: 0;
@@ -211,7 +276,7 @@ nav{
 .bg-content-work-experience {
   height: auto;
   width: 100%;
-  background-color: #D6E6F2;
+  background-color: #222566;
   /* background-image: linear-gradient(rgb(169, 193, 232), #f5f5f5); */
 }
 
@@ -257,17 +322,21 @@ p {
   margin: 0 auto 2rem;
 }
 .decs-topic{
-  color: #303481;
+  color: #f5f5f5;
   font-weight: 600;
   font-size: 1.7rem;
   padding-bottom: 1%;
 }
 .topic-content {
   position: relative;
-  font-weight: 700;
-  font-size: 2.6rem;
+  font-weight: 600;
+  font-size: 2.2rem;
   padding-bottom: 1%;
   /* background-image: linear-gradient(rgb(255, 255, 255,0.2), rgba(194, 207, 228, 0.4)); */
+}
+
+.list-disc {
+  height: 120px;
 }
 
 card {
@@ -280,7 +349,6 @@ card {
   background-color: #ffffff;
   opacity: 0.9;
   max-width: 100%;
-  max-height: 500px;
 }
 
 .header-card {
